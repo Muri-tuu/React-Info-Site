d4938579d58354a5c1fa10e8097c76f2ff13a445
Muri-tuu <kenhopkins001@gmail.com>
2024-06-01 00:11:56 +0300

    Initial Commit

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="img/react-icon-small.png" className="nav--icon" />
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">Intro to React</h4>
+        </nav>
+    )
+}
